dfcdfc18a79b670c9ab6cd769b51a0929de8304b
Ming Liu <mliu@macpro.home>
2019-04-12 12:09:37 -0400

    carousel continues, added index counter and starting position for each image at index change and screen resize

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -3,21 +3,39 @@ class Carousel {
     constructor(element, elementWidth) {
         this.element = element;
         this.width = elementWidth;
-        this.imgs = Array.from(this.element.querySelectorAll("img")).map(elem => new CarouselImg(elem));
+        this.imgs = Array.from(this.element.querySelectorAll("img")).map((elem, index) => new CarouselImg(elem, index, this));
+        this.currentIndex = 0;
         this.imgCount = this.imgs.length;
-        
-
-
+        window.addEventListener("resize", () => {
+            this.width = document.querySelector(".carousel").offsetWidth;
+        })
+        this.left = this.element.querySelector(".left-button");
+        this.left.addEventListener("click", () => this.slide(-1));
+        this.right = this.element.querySelector(".right-button");
+        this.right.addEventListener("click", () => this.slide(+1));
     }
+    //sliding changing current position by +1 or -1, 
     slide(arr) {
-
+        this.currentIndex += arr;
+        if(this.currentIndex >= this.imgCount) {
+            this.currentIndex = 0;
+        } else if(this.currentIndex <= 0 ) {
+            this.currentIndex = this.imgCount-1;
+        }
+        this.imgs.forEach(elem => elem.updateStartingPoint());
     }
 }
 
 class CarouselImg {
-    constructor(element) {
+    constructor(element, index, carousel) {
         this.element = element;
-        console.log(element);
+        this.carousel = carousel;
+        this.index = index;
+        this.startingPoint = (this.index - this.carousel.currentIndex) * this.carousel.width;
+    }
+    updateStartingPoint() {
+        this.startingPoint = (this.index - this.carousel.currentIndex) * this.carousel.width;
+        console.log(this.startingPoint);
     }
 }
 
